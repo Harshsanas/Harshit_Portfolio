c1f0c68504c2b36c4a452db663f99e1a3af194ea
Harshsanas <harrysanas140@gmail.com>
2021-07-29 13:58:28 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-import resumeData from './resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portfolio/>
+        <ContactUs/>
+        <Footer/>
       </div>
     );
   }
