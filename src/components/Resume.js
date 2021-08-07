062961216a2a8a6b7750ec5f3b3f189c9e995165
Harshsanas <harrysanas140@gmail.com>
2021-08-07 17:47:46 +0530

    skills-css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -27,27 +27,6 @@ export default  function Resume() {
         </div>
 
         <section id="resume">
-          {/* <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div> */}
-
-          {/* <div className="row work">
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div> */}
 
           <div className="row skill">
             <div className="three columns header-col">
@@ -67,17 +46,6 @@ export default  function Resume() {
                   <i className="devicon-mongodb-plain-wordmark  technology" />
                   <i className="devicon-nodejs-plain technology" />
                   <i className="devicon-materialui-plain  technology" />
-                  {/*
-              <i className="devicon-postgresql-plain-wordmark  technology"></i>
-              <i className="devicon-webpack-plain colored technology"></i>
-              <i className="devicon-rails-plain-wordmark technology"></i>
-              <i className="devicon-react-plain-wordmark colored technology"></i>
-              <i className="devicon-php-plain technology"></i>
-              <i className="devicon-laravel-plain-wordmark colored technology"></i>
-              <i className="devicon-docker-plain-wordmark technology"></i>
-              <i className="devicon-mysql-plain-wordmark colored technology"></i>
-              <i className="devicon-github-original technology"></i>
-              <i className="devicon-amazonwebservices-plain-wordmark colored technology"></i> */}
                 </div>
               </section>
             </div>
